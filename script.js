b01e4140f3accefc21acd31953f3049fd67b4d46
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-21 18:56:18 +0530

    orgainzed code with best practices

--- script.js
+++ script.js
@@ -12,184 +12,213 @@ let inputText = "";
 let page = 1;
 let timeoutId;
 
+searchInput.addEventListener("input", handleSearchInput);
+prevBtn.addEventListener("click", handlePrevButtonClick);
+nextBtn.addEventListener("click", handleNextButtonClick);
+moviesWrapper.addEventListener("click", handleMovieCardClick);
+
 const fetchMovies = async () => {
-    inputText = searchInput.value;
+  inputText = searchInput.value;
 
-    try {
-        const url = `https://www.omdbapi.com/?&apikey=${API_KEY}&s=${inputText}&page=${page}`;
-        loadingIndicator.classList.remove("hidden");
-        const response = await fetch(url);
+  try {
+    const url = `https://www.omdbapi.com/?&apikey=${API_KEY}&s=${inputText}&page=${page}`;
+    showLoadingIndicator();
 
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
+    const response = await fetch(url);
 
-        const data = await response.json();
-        const results = data.Search;
-
-        // Update pagination visibility
-        if (data.totalResults <= 10) {
-            pagination.classList.add("hidden");
-        } else {
-            pagination.classList.remove("hidden");
-        }
-
-        // Update "Prev" button state
-        if (page === 1) {
-            prevBtn.setAttribute("disabled", "");
-        } else {
-            prevBtn.removeAttribute("disabled");
-        }
-
-        console.log(data);
-
-        if (results == undefined) {
-            errorMessage.textContent = "No Movies Found!";
-            errorMessage.classList.remove("hidden");
-            moviesWrapper.innerHTML = "";
-        } else {
-            if (!errorMessage.classList.contains("hidden")) {
-                errorMessage.classList.add("hidden");
-            }
-            displayMovies(results);
-        }
-    } catch (err) {
-        console.error(err);
-        errorMessage.textContent = "Something went wrong. Please try again later!";
-        errorMessage.classList.remove("hidden");
-    } finally {
-        loadingIndicator.classList.add("hidden");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+
+    const data = await response.json();
+    const results = data.Search;
+
+    updatePaginationVisibility(data.totalResults);
+
+    updatePrevButtonState();
+
+    handleMoviesResults(results);
+  } catch (err) {
+    handleError(err);
+  } finally {
+    hideLoadingIndicator();
+  }
 };
 
-searchInput.addEventListener("input", (e) => {
-    e.preventDefault();
-
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-        fetchMovies();
-    }, 1000);
+function handleSearchInput (e) {
+  e.preventDefault();
+    
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    fetchMovies();
+  }, 1000);
 
     if (!e.target.value) {
-        if (!errorMessage.classList.contains("hidden")) {
-            errorMessage.classList.add("hidden");
-        }
-    }
-});
-
-function displayMovies(data) {
-    const fragment = new DocumentFragment();
-    data.forEach((item) => {
-        let movieCard = document.createElement("div");
-        movieCard.classList.add("movieCard", "w-52", "h-full");
-        movieCard.setAttribute("id", item.imdbID);
-        movieCard.innerHTML = ` <div class="card bg-base-100 h-full shadow-xl cursor-pointer hover:scale-95 transition duration-500 flex flex-col justify-between">
-            <figure class="h-[70%]"><img class="min-h-full" src=${item.Poster} alt="movie" /></figure>
-            <div class="card-body mt-4">
-                <h2 class="card-title w-fit mx-auto text-base text-center font-semibold">${item.Title}</h2>
-                <h2 class="text-xs mt-2 text-center">Released on <span class="font-bold">${item.Year}</span></h2>
-            </div>
-        </div>`;
-        fragment.appendChild(movieCard);
-    });
-
-    moviesWrapper.innerHTML = "";
-    moviesWrapper.appendChild(fragment);
-}
-
-prevBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    page--;
-    moviesWrapper.innerHTML = "";
-    fetchMovies();
-});
-
-nextBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    page++;
-    moviesWrapper.innerHTML = "";
-    fetchMovies();
-});
-
-const fetchMovieDetails = async (id) => {
-    try {
-        const url = `https://www.omdbapi.com/?&apikey=${API_KEY}&i=${id}`;
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-
-        const data = await response.json();
-        console.log(data);
-        showDetails(data);
-    } catch (error) {
-        console.error(error);
-    }
+    clearTimeout(timeoutId);
+    hideErrorMessage();
+  }
 };
 
-moviesWrapper.addEventListener("click", (event) => {
-    const clickedCard = event.target.closest(".movieCard");
-    if (clickedCard) {
-        const id = clickedCard.id;
-        fetchMovieDetails(id);
+function handlePrevButtonClick(e){
+  e.preventDefault();
+  page--;
+  clearMoviesWrapper();
+  fetchMovies();
+};
 
+function handleNextButtonClick (e){
+  e.preventDefault();
+  page++;
+  clearMoviesWrapper();
+  fetchMovies();
+};
+
+function handleMovieCardClick (event){
+  const clickedCard = event.target.closest(".movieCard");
+  if (clickedCard) {
+    const id = clickedCard.id;
+    fetchMovieDetails(id);
+  }
+};
+
+async function fetchMovieDetails(id){
+  try {
+    const url = `https://www.omdbapi.com/?&apikey=${API_KEY}&i=${id}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-});
-function showDetails(data) {
-    const {
-        Poster,
-        Title,
-        Year,
-        Released,
-        Runtime,
-        Genre,
-        Director,
-        Plot,
-        Language,
-        Country,
-        imdbRating,
-    } = data;
-    modalData.innerHTML = `
-<div class="md:flex px-4 leading-none max-w-4xl">
-                    <div class="flex-none ">
-                        <img src=${Poster}
-                            alt=${Title}
-                            class="h-72 w-56 rounded-md shadow-2xl transform -translate-y-4 border-4 border-gray-300 " />
-                    </div>
 
-                    <div class="flex-col text-gray-300 px-4">
+    const data = await response.json();
+    showDetails(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-                        <p class="pt-4 text-2xl font-bold"><span id="title">${Title}</span> | <span id="year">${Year}</span></p>
-                        <hr class="hr-text" data-content="">
-                        <div class="text-md flex justify-between my-2">
-                            <span class="font-bold"><span id="runtime">${Runtime}</span> | <span id="genre">${Genre}</span></span>
-                            <span class="font-bold"></span>
-                        </div>
-                        <p id="plot" class="hidden md:block my-4 text-sm text-left">${Plot}</p>
-                        <p  class="flex text-md my-2">
-                            Released on ${Released}
-                        </p>
-                        <p id="rating" class="flex text-md my-2">
-                            Rating: ${imdbRating}
-                        </p>
-                        <p  class="flex text-md my-2">
-                            Language: ${Language}
-                        </p>
-                        <p  class="flex text-md my-2">
-                            Country: ${Country}
-                        </p>
-                        <p  class="flex text-md my-2">
-                            Director: ${Director}
-                        </p>
-                        
+const showDetails = (data) => {
+  const {
+    Poster,
+    Title,
+    Year,
+    Released,
+    Runtime,
+    Genre,
+    Director,
+    Plot,
+    Language,
+    Country,
+    imdbRating,
+  } = data;
 
-                        
-                    </div>
-                </div>`;
-    detailsModal.showModal();
+  modalData.innerHTML = `
+    <div class="md:flex px-4 leading-none max-w-4xl">
+        <div class="flex-none ">
+            <img src=${Poster}
+                alt=${Title}
+                class="h-72 w-56 rounded-md shadow-2xl transform -translate-y-4 border-4 border-gray-300 " />
+        </div>
 
+        <div class="flex-col text-gray-300 px-4">
 
-}
+            <p class="pt-4 text-2xl font-bold"><span id="title">${Title}</span> | <span id="year">${Year}</span></p>
+            <hr class="hr-text" data-content="">
+            <div class="text-md flex justify-between my-2">
+                <span class="font-bold"><span id="runtime">${Runtime}</span> | <span id="genre">${Genre}</span></span>
+                <span class="font-bold"></span>
+            </div>
+            <p id="plot" class="hidden md:block my-4 text-sm text-left">${Plot}</p>
+            <p  class="flex text-md my-2">
+                Released on ${Released}
+            </p>
+            <p id="rating" class="flex text-md my-2">
+                Rating: ${imdbRating}
+            </p>
+            <p  class="flex text-md my-2">
+                Language: ${Language}
+            </p>
+            <p  class="flex text-md my-2">
+                Country: ${Country}
+            </p>
+            <p  class="flex text-md my-2">
+                Director: ${Director}
+            </p>
 
+            
+        </div>
+    </div>`;
+  detailsModal.showModal();
+};
 
+const showLoadingIndicator = () => {
+  loadingIndicator.classList.remove("hidden");
+};
+
+const hideLoadingIndicator = () => {
+  loadingIndicator.classList.add("hidden");
+};
+
+const updatePaginationVisibility = (totalResults) => {
+  pagination.classList.toggle("hidden", totalResults <= 10);
+};
+
+const updatePrevButtonState = () => {
+  page === 1
+    ? prevBtn.setAttribute("disabled", "")
+    : prevBtn.removeAttribute("disabled");
+};
+
+const handleError = (err) => {
+  console.error(err);
+  errorMessage.textContent = "Something went wrong. Please try again later!";
+  errorMessage.classList.remove("hidden");
+};
+
+const hideErrorMessage = () => {
+  errorMessage.classList.add("hidden");
+};
+
+const clearMoviesWrapper = () => {
+  moviesWrapper.innerHTML = "";
+};
+
+const handleMoviesResults = (results) => {
+  if (results === undefined) {
+    errorMessage.textContent = "No Movies Found!";
+    errorMessage.classList.remove("hidden");
+    clearMoviesWrapper();
+  } else {
+    hideErrorMessage();
+    displayMovies(results);
+  }
+};
+
+const displayMovies = (data) => {
+  const fragment = new DocumentFragment();
+  data.forEach((item) => {
+    const movieCard = createMovieCard(item);
+    fragment.appendChild(movieCard);
+  });
+
+  clearMoviesWrapper();
+  moviesWrapper.appendChild(fragment);
+};
+
+const createMovieCard = (item) => {
+  const movieCard = document.createElement("div");
+  movieCard.classList.add("movieCard", "w-52", "h-full");
+  movieCard.setAttribute("id", item.imdbID);
+  movieCard.innerHTML = `
+    <div class="card bg-base-100 h-full shadow-xl cursor-pointer hover:scale-95 transition duration-500 flex flex-col justify-between">
+        <figure class="h-[70%]"><img class="min-h-full" src=${item.Poster} alt="movie" /></figure>
+        <div class="card-body mt-4">
+            <h2 class="card-title w-fit mx-auto text-base text-center font-semibold">${item.Title}</h2>
+            <h2 class="text-xs mt-2 text-center">Released on <span class="font-bold">${item.Year}</span></h2>
+        </div>
+    </div>`;
+  return movieCard;
+};
+
+// Initial fetch
+// fetchMovies();
